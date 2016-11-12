8ed43ebb46719986ea17443d4a92300b0d922b6d
Jayjay Lumanang <jayshiro@gmail.com>
2016-11-12 11:38:18 +1100

    First simple test on payslip service class

--- src/test/java/com/lumanang/jayjay/service/PayslipServiceImplTest.java
+++ src/test/java/com/lumanang/jayjay/service/PayslipServiceImplTest.java
@@ -1,4 +1,23 @@
 package com.lumanang.jayjay.service;
 
+import com.lumanang.jayjay.model.Employee;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PayslipServiceImplTest {
+
+    private PayslipService payslipService;
+
+    @Before
+    public void setup() {
+        Employee employee = new Employee("John","Doe",70000,8,"01 September - 30 September");
+        payslipService = new PayslipServiceImpl(employee);
+    }
+
+    @Test
+    public void shouldReturnPayPeriodEqualsPaymentStartDate() {
+        assertEquals(payslipService.getPayPeriod(), "01 September - 30 September");
+    }
 }
