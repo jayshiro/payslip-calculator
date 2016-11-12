e308298b0f408f3eb144eb79f6e98612116acd29
Jayjay Lumanang <jayshiro@gmail.com>
2016-11-12 11:20:52 +1100

    Added the model classes

---
+++ src/main/java/com/lumanang/jayjay/model/Employee.java
@@ -0,0 +1,49 @@
+package com.lumanang.jayjay.model;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private double annualSalary;
+    private double superRate;
+    private String paymentStartDate;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getAnnualSalary() {
+        return annualSalary;
+    }
+
+    public void setAnnualSalary(double annualSalary) {
+        this.annualSalary = annualSalary;
+    }
+
+    public double getSuperRate() {
+        return superRate;
+    }
+
+    public void setSuperRate(double superRate) {
+        this.superRate = superRate;
+    }
+
+    public String getPaymentStartDate() {
+        return paymentStartDate;
+    }
+
+    public void setPaymentStartDate(String paymentStartDate) {
+        this.paymentStartDate = paymentStartDate;
+    }
+}
---
+++ src/main/java/com/lumanang/jayjay/model/Payslip.java
@@ -0,0 +1,13 @@
+package com.lumanang.jayjay.model;
+
+public class Payslip {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
