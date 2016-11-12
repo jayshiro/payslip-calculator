a60920ba2f1438922b39a192c4a35b8e1aad3ec2
Jayjay Lumanang <jayshiro@gmail.com>
2016-11-12 13:05:08 +1100

    Merged NormalPayslip and Payslip into Payslip to make it simpler

--- src/main/java/com/lumanang/jayjay/model/Payslip.java
+++ src/main/java/com/lumanang/jayjay/model/Payslip.java
@@ -1,7 +1,13 @@
 package com.lumanang.jayjay.model;
 
 public class Payslip {
+
     private String name;
+    private String payPeriod;
+    private double grossIncome;
+    private double incomeTax;
+    private double netIncome;
+    private double superAnnuation;
 
     public String getName() {
         return name;
@@ -9,5 +15,45 @@ public class Payslip {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getPayPeriod() {
+        return payPeriod;
+    }
+
+    public void setPayPeriod(String payPeriod) {
+        this.payPeriod = payPeriod;
+    }
+
+    public double getGrossIncome() {
+        return grossIncome;
+    }
+
+    public void setGrossIncome(double grossIncome) {
+        this.grossIncome = grossIncome;
+    }
+
+    public double getIncomeTax() {
+        return incomeTax;
+    }
+
+    public void setIncomeTax(double incomeTax) {
+        this.incomeTax = incomeTax;
+    }
+
+    public double getNetIncome() {
+        return netIncome;
+    }
+
+    public void setNetIncome(double netIncome) {
+        this.netIncome = netIncome;
+    }
+
+    public double getSuperAnnuation() {
+        return superAnnuation;
+    }
+
+    public void setSuperAnnuation(double superAnnuation) {
+        this.superAnnuation = superAnnuation;
     }
 }
